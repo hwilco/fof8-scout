ac5b485d37760f386dbd895cd286ea7442d26825
Harrison Wilco <hwilco@gmail.com>
2026-04-28 16:44:44 -0400

    Phase 5: Quarantine Exploratory Code - Moved keep scripts to scripts/ and deleted obsolete scratch files

--- fof8-ml/scratch/check_columns.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-from fof8_core.loader import FOF8Loader
-from fof8_core.features import get_draft_class
-
-loader = FOF8Loader(base_path="/workspaces/fof8-scout/fof8-gen/data/raw", league_name="DRAFT003")
-df = get_draft_class(loader, 2021)
-print(df.columns)
